b2d9245ed4e65110ddc866d5fbb854c82bf27ebd
yongwoo <pure_yong@naver.com>
2021-11-19 16:39:29 +0900

    context file yaml 전환 개발

--- src/com/yong/common/Configuration.java
+++ src/com/yong/common/Configuration.java
@@ -10,7 +10,9 @@ import org.apache.log4j.PropertyConfigurator;
 import org.yaml.snakeyaml.Yaml;
 
 import com.yong.msg.MsgCodeConfiguration;
+import com.yong.util.CommonUtil;
 
+@SuppressWarnings({ "unused", "rawtypes" })
 public class Configuration {
 
 	private final Logger logger =  Logger.getLogger(Configuration.class);
@@ -20,6 +22,7 @@ public class Configuration {
 			logger.info("Complete to load log4j properties file from " + MsgCodeConfiguration.MSG_VALUE_PATH_LOG4J);
 			logger.info("Trying to parse application.yml to start this program!");
 			this.parseApplicationYml();
+			logger.info("Complete to load application.yml!");
 	}
 	
 	private void parseApplicationYml() {
@@ -27,14 +30,39 @@ public class Configuration {
 			Map<String, Object> yml = new Yaml().load(new FileReader(MsgCodeConfiguration.MSG_VALUE_PATH_APPLICATION));
 			List envList = (List) ((Map) yml.get("environment")).get("envList");
 			logger.info("There is " + envList.size() + " environment in application.yml");
+			
+			int index = 1;
 			for(Object env : envList) {
-				logger.info(env.toString());
+				logger.info("env" + index++ + " : " + env.toString());
+				logger.info("  - local.port : " + this.getValue(env, new String[] {"local","port"}));
+				logger.info("  - remote.host : " + this.getValue(env, new String[] {"remote","host"}));
+				logger.info("  - remote.port : " + this.getValue(env, new String[] {"remote","port"}));
+				logger.info("  - remote.key : " + this.getValue(env, new String[] {"remote","key"}));
+				if(((Map) env).get("mysql") != null) {
+					logger.info("  - mysql.host : " + this.getValue(env, new String[] {"mysql","host"}));
+					logger.info("  - mysql.port : " + this.getValue(env, new String[] {"mysql","port"}));
+					logger.info("  - mysql.id : " + this.getValue(env, new String[] {"mysql","id"}));
+					logger.info("  - mysql.pwd : " + this.getValue(env, new String[] {"mysql","pwd"}));
+					logger.info("  - mysql.test.test : " + this.getValue(env, new String[] {"mysql","test","test"}));
+				} else if (((Map) env).get("command") != null) {
+					logger.info("  - command.line : " + this.getValue(env, new String[] {"command","line"}));
+				} else {
+					logger.error("Wrong Type");
+				}
 			}
 			
 		}catch (FileNotFoundException e) { 
 			e.printStackTrace(); 
 			logger.error("There is a exception in progress : " + e.toString());
 		}	
+	}
+	
+	private Object getValue(Object obj, String[] keys) {
+		Object tt = obj;
+		for(String key : keys) {
+			tt = CommonUtil.ObjToMap(tt).get(key);
+		}
+		return tt;
 	}
 	
 }
--- src/com/yong/util/CommonUtil.java
+++ src/com/yong/util/CommonUtil.java
@@ -1,5 +1,21 @@
 package com.yong.util;
 
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings("rawtypes")
 public class CommonUtil {
 
+	public static Map ObjToMap(Object obj) {
+		return (Map) obj;
+	}
+	
+	public static List ObjToList(Object obj) {
+		return (List) obj;
+	}
+	
+	public static String ObjToString(Object obj) {
+		return (String) obj;
+	}
+	
 }
