beb86ce99de83e2c65885a3d852b71c4b6c86578
yongwoo <pure_yong@naver.com>
2021-11-19 15:55:43 +0900

    context file yaml 전환 개발

--- src/com/yong/common/Configuration.java
+++ src/com/yong/common/Configuration.java
@@ -2,6 +2,7 @@ package com.yong.common;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
@@ -13,17 +14,22 @@ import com.yong.msg.MsgCodeConfiguration;
 public class Configuration {
 
 	private final Logger logger =  Logger.getLogger(Configuration.class);
-	
+		
 	public void initialize() throws Exception {
 			PropertyConfigurator.configure(MsgCodeConfiguration.MSG_VALUE_PATH_LOG4J);
 			logger.info("Complete to load log4j properties file from " + MsgCodeConfiguration.MSG_VALUE_PATH_LOG4J);
-			
 			logger.info("Trying to parse application.yml to start this program!");
+			this.parseApplicationYml();
 	}
 	
 	private void parseApplicationYml() {
 		try {
-			Map<String, Object> propMap = new Yaml().load(new FileReader(MsgCodeConfiguration.MSG_VALUE_PATH_APPLICATION));
+			Map<String, Object> yml = new Yaml().load(new FileReader(MsgCodeConfiguration.MSG_VALUE_PATH_APPLICATION));
+			List envList = (List) ((Map) yml.get("environment")).get("envList");
+			logger.info("There is " + envList.size() + " environment in application.yml");
+			for(Object env : envList) {
+				logger.info(env.toString());
+			}
 			
 		}catch (FileNotFoundException e) { 
 			e.printStackTrace(); 
