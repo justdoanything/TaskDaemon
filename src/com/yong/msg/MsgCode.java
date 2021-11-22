6a2241b4e2dcc070c6d01f19b86518d11cb5b996
yongwoo <pure_yong@naver.com>
2021-11-22 14:24:25 +0900

    MsgCode 수정

--- src/com/yong/msg/MsgCode.java
+++
@@ -1,7 +0,0 @@
-package com.yong.util;
-
-public class MsgCode {
-	
-	public static String CONF_KEY_PATH_LOG4J = "CONF_PATH_LOG4J";
-	public static String CONF_KEY_TIME_INTERVAL = "CONF_TIME_INTERVAL";
-}
--- src/com/yong/msg/MsgCodeConfiguration.java
+++ src/com/yong/msg/MsgCodeConfiguration.java
@@ -3,4 +3,9 @@ package com.yong.msg;
 public class MsgCodeConfiguration {
 	public static final String MSG_VALUE_PATH_APPLICATION = "./conf/application.yml";
 	public static final String MSG_VALUE_PATH_LOG4J = "./conf/log4j.properties";
+	
+	public static final String MSG_WORD_KEY_ENVIRONMENT = "environment";
+	public static final String MSG_WORD_KEY_ENVLIST = "envList";
+	public static final String MSG_WORD_KEY_ENV = "env";
 }
+
